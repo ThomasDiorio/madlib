86933e6ada342be5f7357409793f56239011bcf4
Thomas Diorio <trd9077@gmail.com>
2021-01-12 17:53:31 -0600

    new commit

---
+++ guess.py
@@ -0,0 +1,9 @@
+noun1 = input("Enter a noun: ")
+noun2 = input("Enter a noun: ")
+verb1 = input("Enter a verb: ")
+verb2 = input("Enter a verb: ")
+
+def madLib(noun1,noun2,verb1,verb2):
+    print(f"The quick {noun1} jumped over the slow {noun2} and would {verb1} all the way home and {verb2}")
+
+madLib(noun1,noun2,verb1,verb2)
