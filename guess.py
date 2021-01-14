19dc34fefaa8f5b7ac071a8649a151e907e1b2a7
Thomas Diorio <trd9077@gmail.com>
2021-01-13 22:36:52 -0600

    removed exclamation  points

--- guess.py
+++ guess.py
@@ -1,4 +1,4 @@
-# This is a Mad Lib game, enjoy!!!.
+# This is a Mad Lib game, enjoy.
 
 noun1 = input("Enter a noun: ")
 noun2 = input("Enter a noun: ")
@@ -7,7 +7,8 @@ verb2 = input("Enter a verb: ")
 
 
 def madLib(noun1, noun2, verb1, verb2):
-    print(f"The quick {noun1} jumped over the slow {noun2} and would {verb1} all the way home and {verb2}.")
+    print(
+        f"The quick {noun1} jumped over the slow {noun2} and would {verb1} all the way home and {verb2}.")
 
 
 madLib(noun1, noun2, verb1, verb2)
